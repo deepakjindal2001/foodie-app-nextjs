c9c69567f7a4399a72da20d007da833472b067a6
Deepak <deepak.jindal2001@gmail.com>
2025-05-05 19:44:59 +0530

    create community page and refactor main header

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -5,6 +5,7 @@ import MainHeaderBackground from "./main-header-background";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import Image from "next/image";
+import NavLink from "./nav-link";
 
 const MainHeader = () => {
   return (
@@ -19,10 +20,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="/community">Foodies Comminity</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
