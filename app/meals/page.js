d5e750d5ce32e0f38ede267132e06ac4f63f5eae
Deepak <deepak.jindal2001@gmail.com>
2025-05-05 20:07:15 +0530

    create meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,26 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
+
 const MealsPage = () => {
   return (
-    <main>
-      <h1>Meals Page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose your favourite recipe and cook it yourself. Is is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favourite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 };
 
