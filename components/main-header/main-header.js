7f750e3cb0ad9198ee37bc731c1ef17cdf503fb2
Deepak <deepak.jindal2001@gmail.com>
2025-05-05 18:07:15 +0530

    Add element on landing page

---
+++ components/main-header/main-header.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+
+import MainHeaderBackground from "./main-header-background";
+
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+
+const MainHeader = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          Next Level Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Comminity</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default MainHeader;
